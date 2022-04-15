b39fec6ccc48cba9685c9d6d6c58c99734bd6167
uguumur <uguumur.oldokh@gmail.com>
2022-04-15 11:16:48 +0800

    added more detail

--- home.js
+++ home.js
@@ -3,15 +3,18 @@ function hide(element) {
   element.remove();
 }
 
+// 2. box уудаа хүссэн тоогоор үүсгэдэг функц
+function createBox(number) {
+  const container = document.getElementById("container");
+  container.innerHTML = '<div class="box red" onclick="hide(this)"></div>';
+}
+
+createBox(5);
+
 const boxList = document.getElementsByClassName("box");
 
 for (var i = 0; i < boxList.length; i++) {
-  boxList[i].style.left = Math.floor(Math.random() * 650);
-  boxList[i].style.top = Math.floor(Math.random() * 450);
+  boxList[i].style.left = Math.floor(Math.random() * 650) + "px";
+  boxList[i].style.top = `${Math.floor(Math.random() * 450)}px`;
   // 1. random color
-}
-
-// 2. box уудаа хүссэн тоогоор үүсгэдэг функц
-function createBox(number) {
-  // TODO
 }
